a3603e4e710bd074b0f6a8c7c5dd4c4af57b8e13
gamer191 <83270075+gamer191@users.noreply.github.com>
2023-09-03 22:53:03 +1000

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
-curl https://github.com/yt-dlp/yt-dlp-nightly-builds/releases/latest/download/yt-dlp.exe > yt-dlp.exe
-curl https://github.com/yt-dlp/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip > ffmpeg.zip
+aria2c -Z https://github.com/yt-dlp/yt-dlp-nightly-builds/releases/latest/download/yt-dlp.exe https://github.com/yt-dlp/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip
 echo --batch-file - > config.txt
-zip yt-dlp.exe ffmpeg.zip config.txt -d final.zip
-rm yt-dlp.exe ffmpeg.zip config.txt
-
+unzip ffmpeg-master-latest-win64-gpl.zip -d ffmpeg
+mv ffmpeg/ffmpeg-master-latest-win64-gpl/bin/ffmpeg.exe ffmpeg.exe
+mv ffmpeg/ffmpeg-master-latest-win64-gpl/bin/ffprobe.exe ffprobe.exe
+mv ffmpeg/ffmpeg-master-latest-win64-gpl/bin/ffplay.exe ffplay.exe
+zip final.zip yt-dlp.exe ffmpeg.exe ffprobe.exe ffplay.exe config.txt
+rm -rf ffmpeg ffmpeg-master-latest-win64-gpl.zip ffmpeg.exe ffprobe.exe ffplay.exe config.txt yt-dlp.exe
